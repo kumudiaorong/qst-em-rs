7f27fe694956b199923a94939eaa64418061f611
kumudiaorong <kmdr.error@gmail.com>
2024-01-11 16:24:35 +0800

    tidy

---
+++ build.rs
@@ -0,0 +1,12 @@
+extern crate tonic_build;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .type_attribute("ExtInfo", "#[derive(serde::Deserialize, serde::Serialize)]")
+        // .out_dir("src/comm")
+        .compile(
+            &["common.proto", "daemon.proto", "extension.proto"],
+            &["qst-grpc/src"],
+        )?;
+    Ok(())
+}
---
+++ src/def.rs
@@ -0,0 +1,6 @@
+pub(super) mod common {
+    tonic::include_proto!("common");
+}
+pub(super) mod daemon {
+    tonic::include_proto!("daemon");
+}
